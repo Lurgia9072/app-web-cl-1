4fbb4661dc7b611a372cff9d61dfc72a31bbe247
lia <93041881+Lurgia9072@users.noreply.github.com>
2022-09-29 04:06:00 -0500

    ajustes

---
+++ exame/src/main/java/model/Alumno.java
@@ -0,0 +1,91 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the alumnos database table.
+ * 
+ */
+@Entity
+@Table(name="alumnos")
+@NamedQuery(name="Alumno.findAll", query="SELECT a FROM Alumno a")
+public class Alumno implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private int codigo;
+
+	private String apellido;
+
+	private int ciclo;
+
+	private int codcarrera;
+
+	private String dni;
+
+	private int edad;
+
+	private String nombre;
+
+	public Alumno() {
+	}
+
+	public int getCodigo() {
+		return this.codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getApellido() {
+		return this.apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getCiclo() {
+		return this.ciclo;
+	}
+
+	public void setCiclo(int ciclo) {
+		this.ciclo = ciclo;
+	}
+
+	public int getCodcarrera() {
+		return this.codcarrera;
+	}
+
+	public void setCodcarrera(int codcarrera) {
+		this.codcarrera = codcarrera;
+	}
+
+	public String getDni() {
+		return this.dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public int getEdad() {
+		return this.edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
